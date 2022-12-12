d5745213e75762ec5cebe7387e81a7d9c34a5847
onoteruaki <terusan.o@icloud.com>
2022-12-12 20:49:06 +0900

    initial commit

--- intern/settings.py
+++ intern/settings.py
@@ -141,6 +141,7 @@ MEDIA_URL = '/media/'
 LOGIN_URL = '/login'
 LOGIN_REDIRECT_URL = '/friends'
 LOGOUT_REDIRECT_URL = '/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
